7fb9e9eff788ecd3ed5af7fe789e91ec7a1e8268
Geoffrey Brier <geoffrey@widop.com>
2013-11-17 12:46:00 +0100

    Add trends support

---
+++ src/Widop/Twitter/Trends/TrendsClosestRequest.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Wid'op package.
+ *
+ * (c) Wid'op <contact@widop.com>
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Widop\Twitter\Trends;
+
+use Widop\Twitter\AbstractRequest;
+use Widop\Twitter\Options\OptionBag;
+
+/**
+ * Trends closest request.
+ *
+ * @link https://dev.twitter.com/docs/api/1.1/get/trends/closest
+ *
+ * @method string getLat()              Gets the latitude.
+ * @method null   setLat(string $lat)   Sets the latitude.
+ * @method string getLong()             Gets the longitude.
+ * @method null   setLong(string $long) Sets the longitude.
+ *
+ * @author Geoffrey Brier <geoffrey.brier@gmail.com>
+ */
+class TrendsClosestRequest extends AbstractRequest
+{
+    /**
+     * Creates a trends closest request.
+     *
+     * @param string $latitude  The latitude.
+     * @param string $longitude The longitude.
+     */
+    public function __construct($latitude, $longitude)
+    {
+        parent::__construct();
+
+        $this->setLat($latitude);
+        $this->setLong($longitude);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureOptionBag(OptionBag $optionBag)
+    {
+        $optionBag
+            ->register('lat')
+            ->register('long');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function validateOptionBag(OptionBag $optionBag)
+    {
+        if (!isset($optionBag['lat'])) {
+            throw new \RuntimeException('You must specify a latitude.');
+        }
+
+        if (!isset($optionBag['long'])) {
+            throw new \RuntimeException('You must specify a longitude.');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPath()
+    {
+        return '/trends/closest.json';
+    }
+}
---
+++ src/Widop/Twitter/Trends/TrendsPlaceRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Wid'op package.
+ *
+ * (c) Wid'op <contact@widop.com>
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Widop\Twitter\Trends;
+
+use Widop\Twitter\AbstractRequest;
+use Widop\Twitter\Options\OptionBag;
+
+/**
+ * Trends place request.
+ *
+ * @link https://dev.twitter.com/docs/api/1.1/get/trends/place
+ * @link http://developer.yahoo.com/geo/geoplanet/
+ *
+ * @method string      getId()                     Gets the WOEID.
+ * @method null        setId(string $id)           Sets the WOEID.
+ * @method string|null getExclude()                Gets if the hashtags will be removed from the trend list.
+ * @method null        setExclude(string $exclude) Sets if the hashtags will be removed from the trend list.
+ *
+ * @author Geoffrey Brier <geoffrey.brier@gmail.com>
+ */
+class TrendsPlaceRequest extends AbstractRequest
+{
+    /**
+     * Creates a trends place request.
+     *
+     * @param string $id The WOEID.
+     */
+    public function __construct($id)
+    {
+        parent::__construct();
+
+        $this->setId($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureOptionBag(OptionBag $optionBag)
+    {
+        $optionBag
+            ->register('id')
+            ->register('exclude');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function validateOptionBag(OptionBag $optionBag)
+    {
+        if (!isset($optionBag['id'])) {
+            throw new \RuntimeException('You must specify a WOEID.');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPath()
+    {
+        return '/trends/place.json';
+    }
+}
